363d6f894a987850cdf75cd9cef6e429795719b7
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-06-22 15:32:28 +0200

    added route to get single gallery depending on name

--- routers/galleries.js
+++ routers/galleries.js
@@ -17,4 +17,17 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/:name", async (req, res, next) => {
+  try {
+    const gallery = await User.findOne({
+      where: { name: req.params.name },
+      include: [Photo],
+    });
+    delete gallery.dataValues["password"];
+    res.send(gallery);
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
