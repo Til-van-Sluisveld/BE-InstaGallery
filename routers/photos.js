052e4ac5b5c32c06a4fa9ab3dea53d30e0c3081b
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-08-20 17:29:16 +0200

    Connected to FE & entry in DB gets deleted

--- routers/photos.js
+++ routers/photos.js
@@ -23,7 +23,17 @@ router.post("/new", authMiddleware, async (req, res, next) => {
 });
 
 router.delete("/delete/:id", async (req, res, next) => {
-  console.log("delete photo with id:", req.params.id);
+  //console.log("delete photo with id:", req.params.id);
+  try {
+    const photoToDelete = await Photo.destroy({ where: { id: req.params.id } });
+    if (photoToDelete) {
+      res.send("received and destoryed");
+    } else {
+      res.send("is already destroyed");
+    }
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
